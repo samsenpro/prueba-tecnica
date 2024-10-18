cb6c7365073c5101b9854d6a74dcd2bace36a245
Samuel Martinez <samueldavidmartinez0@gmail.com>
2024-10-18 01:07:29 -0500

    quitar el boton de eliminar por confusion del dashboard

--- administrativo/js/FuncionesDashboard.js
+++ administrativo/js/FuncionesDashboard.js
@@ -30,7 +30,6 @@ function cargarTareas() {
                                 <p class="card-text"><strong>Prioridad:</strong> ${prioridadCapitalizada}</p>
                                 <p class="card-text"><strong>Estado:</strong> ${estadoCapitalizado}</p> <!-- Muestra el estado capitalizado -->
                                 <button class="btn btn-primary" onclick="cambiarEstado(${tarea.id})">Marcar como Completada</button>
-                                <button class="btn btn-danger" onclick="confirmarEliminar(${tarea.id})">Eliminar</button>
                             </div>
                         </div>
                     </div>
@@ -71,31 +70,4 @@ function cambiarEstado(id) {
         console.error('Error al actualizar el estado:', error);
         alert('Error al actualizar el estado');
     });
-}
-
-function confirmarEliminar(id) {
-    const modalHtml = `
-        <div class="modal fade" id="confirmarEliminarModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="modalLabel">Confirmación de Eliminación</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body text-center">
-                        <i class="bi bi-exclamation-triangle-fill" style="font-size: 50px; color: red;"></i>
-                        <p>¿Estás seguro de que deseas eliminar esta tarea?</p>
-                    </div>
-                    <div class="modal-footer justify-content-center">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                        <button type="button" class="btn btn-danger" onclick="eliminarTarea(${id})">Eliminar</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    `;
-
-    document.body.insertAdjacentHTML('beforeend', modalHtml);
-    const modal = new bootstrap.Modal(document.getElementById('confirmarEliminarModal'));
-    modal.show();
 }
